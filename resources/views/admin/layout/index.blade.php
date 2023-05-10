b118490f8e03f525206bec6d33ebc622a6cdd99e
Aounhassan275 <aounhassan275@gmail.com>
2023-05-10 14:29:31 +0500

    fix

--- resources/views/admin/layout/index.blade.php
+++ resources/views/admin/layout/index.blade.php
@@ -191,24 +191,24 @@
 								<span>Student Attendance</span>
 							</a>
 						</li>
-						<li class="nav-item nav-item-submenu {{Request::is('admin/exam*') || Request::is('admin/grade*') || Request::is('admin/grade_category*')  ?'nav-item-open':''}}">
+						<li class="nav-item nav-item-submenu {{Request::is('admin/exam*') || Request::is('admin/grade*') || Request::is('admin/grade_category*')  || Request::is('admin/entrance_fee*')  ?'nav-item-open':''}}">
 							<a href="#" class="nav-link"><i class="icon-compose"></i> <span>Exam</span></a>
 
-							<ul class="nav nav-group-sub" data-submenu-title="Layouts" style="{{Request::is('admin/exam*')?'display:block':''}}">
+							<ul class="nav nav-group-sub" data-submenu-title="Layouts" style="{{Request::is('admin/exam*') || Request::is('admin/entrance_fee*')?'display:block':''}}">
 								<li class="nav-item"><a href="{{route('admin.exam.index')}}" class="nav-link {{Request::is('admin/exam')?'active':''}}">Exam</a></li>
 								<li class="nav-item"><a href="{{route('admin.grade_category.index')}}" class="nav-link {{Request::is('admin/grade_category')?'active':''}}">Grade Category</a></li>
 								<li class="nav-item"><a href="{{route('admin.grade.index')}}" class="nav-link {{Request::is('admin/grade')?'active':''}}">Grade</a></li>
+								<li class="nav-item"><a href="{{route('admin.entrance_fee.index')}}" class="nav-link {{Request::is('admin/entrance_fee')?'active':''}}">Entrance Fee</a></li>
 							</ul>
 						</li>
-						<li class="nav-item nav-item-submenu {{Request::is('admin/country*') || Request::is('admin/state*') || Request::is('admin/city*')  || Request::is('admin/police_station*') || Request::is('admin/entrance_fee*') ?'nav-item-open':''}}">
+						<li class="nav-item nav-item-submenu {{Request::is('admin/country*') || Request::is('admin/state*') || Request::is('admin/city*')  || Request::is('admin/police_station*') ?'nav-item-open':''}}">
 							<a href="#" class="nav-link"><i class="icon-map"></i> <span>Locations</span></a>
 
-							<ul class="nav nav-group-sub" data-submenu-title="Layouts" style="{{Request::is('admin/country*') || Request::is('admin/state*') || Request::is('admin/city*') || Request::is('admin/police_station*')|| Request::is('admin/entrance_fee*')?'display:block':''}}">
+							<ul class="nav nav-group-sub" data-submenu-title="Layouts" style="{{Request::is('admin/country*') || Request::is('admin/state*') || Request::is('admin/city*') || Request::is('admin/police_station*')?'display:block':''}}">
 								<li class="nav-item"><a href="{{route('admin.country.index')}}" class="nav-link {{Request::is('admin/country')?'active':''}}">Country</a></li>
 								<li class="nav-item"><a href="{{route('admin.state.index')}}" class="nav-link {{Request::is('admin/state')?'active':''}}">State</a></li>
 								<li class="nav-item"><a href="{{route('admin.city.index')}}" class="nav-link {{Request::is('admin/city')?'active':''}}">City</a></li>
 								<li class="nav-item"><a href="{{route('admin.police_station.index')}}" class="nav-link {{Request::is('admin/police_station')?'active':''}}">Police Station</a></li>
-								<li class="nav-item"><a href="{{route('admin.entrance_fee.index')}}" class="nav-link {{Request::is('admin/entrance_fee')?'active':''}}">Entrance Fee</a></li>
 							</ul>
 						</li>
 					</ul>
